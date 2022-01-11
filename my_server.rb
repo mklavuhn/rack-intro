41f1e731b74592b38bf79d7d4f40450b919b86d4
mklavuhn <michaelklavuhn3@gmail.com>
2022-01-10 16:08:26 -0800

    commit tree 91badabb4550c2ca966f2556620d259b64f51c96

---
+++ my_server.rb
@@ -0,0 +1,19 @@
+require 'rack'
+
+
+
+
+class MyServer 
+    def call(env)
+        return [200, {'Content-Type' => 'text/html'}, pretty_response]
+    end
+
+    def pretty_response
+        (Time.now.to_i % 2).zero? ? ['<em>Hello</em>'] : ['<strong>Hello</strong>']
+    end
+end
+
+my_server = Proc.new do
+    [200, { 'Content-Type' => 'text/html'}, ['<em>Hello</em>']]
+end
+
